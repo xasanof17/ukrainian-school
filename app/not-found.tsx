f6ddd93eb62a2221a7e1615626a4ea248159ff65
Javohir <xasanof17@gmail.com>
2023-09-07 19:24:09 +0500

    not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function NotFoundPage() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <h1>404! Not Found Page</h1>
+      <h1 className="text-3xl font-bold text-primary mb-5">
+        404! Not Found Page
+      </h1>
       <Link href="/" className="btn-secondary">
         Home
       </Link>
